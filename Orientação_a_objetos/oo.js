14218ad94bdc34b516047495c4fc1406aae66751
EduartePaiva <eduarte2018.po@gmail.com>
2023-01-09 16:23:29 -0300

    aula
    oo

---
+++ Orientação_a_objetos/oo.js
@@ -0,0 +1,19 @@
+// código não executável!!!
+
+// Procedual
+processamento(valor1,valor2,valor3)
+
+// OO 
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+// Principios importantes:
+// 1. abstração
