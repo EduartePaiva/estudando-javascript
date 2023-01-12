2341fe08bfd9796911443264abcf2312ba2a9f0f
EduartePaiva <eduarte2018.po@gmail.com>
2023-01-11 23:53:43 -0300

    Update calculo_dkzona.js
    dk

--- Orientação_a_objetos/calculo_dkzona.js
+++ Orientação_a_objetos/calculo_dkzona.js
@@ -1,13 +1,21 @@
 //cap de dkzona é 580 a partir disto tem 70% de eficiência
 //dano kamasylvia tem 85% de eficiência depois do cap
 
-function calcular_ap_total_dkzona(dano_kama,dano_monstro,cap){
+let dkzona = 0.7
+let trolls = 0.05
+let cap_dkzona = 580
+let cap_trolls = 920
+
+function calcular_ap_total_dkzona(dano_kama,dano_monstro,cap,cap_porcentagem_spot){
     let dano_total = 0
     if(dano_monstro > cap){
-        dano_total = cap + (dano_monstro-cap)*.7
+        dano_total = cap + (dano_monstro-cap)*cap_porcentagem_spot
         dano_total += dano_kama*0.85
+    }else if((dano_monstro+dano_kama) > cap){
+        dano_total = cap + ((dano_monstro+dano_kama)-cap)*0.85
     }else{
         dano_total = dano_monstro+dano_kama
+        console.log('aq')
     }
 
     return dano_total
@@ -15,5 +23,5 @@ function calcular_ap_total_dkzona(dano_kama,dano_monstro,cap){
 }
 
 
-console.log('Dano com kutum: '+calcular_ap_total_dkzona(53,785,580))
-console.log('Dano com secundaria de precisão: '+calcular_ap_total_dkzona(83,694,580))
+console.log('Dano com kutum: '+calcular_ap_total_dkzona(73,919,cap_trolls,trolls))
+console.log('Dano com secundaria de precisão: '+calcular_ap_total_dkzona(73,818,cap_trolls,trolls))
