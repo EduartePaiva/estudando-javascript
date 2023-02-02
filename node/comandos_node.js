84fc45dd3cadab9751ac1b2966052ddd6067b7ac
EduartePaiva <eduarte2018.po@gmail.com>
2023-02-01 22:20:11 -0300

    MAIS NODE
    AA

---
+++ node/comandos_node.js
@@ -0,0 +1,44 @@
+/*
+
+npm install -g nodemon
+instala o nodemon globalmente
+
+npm i
+instala o que tiver no package.json
+
+cd caminho/caminho
+entra em uma pasta
+cd ../
+sai da pasta para a anterior
+
+
+PARAR O NODEMON É CRTL+C
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
