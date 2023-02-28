c178287b4275f3e0226eab2f22035999be22fb15
EduartePaiva <eduarte2018.po@gmail.com>
2023-02-28 16:15:52 -0300

    alterações
    remocao do meutcc

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,35 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TeserPlugin = require('terser-webpack-plugin') //mimificar javascript
+const MinimizerCssPlugin = require('css-minimizer-webpack-plugin') //mimificar css
+const path = require('path')
+
+
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
-        path: __dirname + '/public'
+        path: __dirname + '/public',
+        assetModuleFilename: 'imagem[hash][ext][query]'
+
+    },
+    devServer: {
+        static:{
+            directory: path.join(__dirname,'public')
+        },
+        compress: true,
+        port: 6000,
+    },
+    optimization: {
+        minimizer: [
+            new TeserPlugin({
+                parallel: true
+            }),
+            new MinimizerCssPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
@@ -22,6 +45,12 @@ module.exports = {
                 'css-loader', //interpretar @import, url()...
                 'sass-loader'
             ]
-        }]
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            type: 'asset/resource',
+            generator:{
+                filename: 'imgs/teste5[hash][ext][query]'
+            }
+        }],
     }
 }
