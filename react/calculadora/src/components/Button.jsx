d959c9541dd616bcbff3c4bfab22663e9962cf6d
EduartePaiva <eduarte2018.po@gmail.com>
2023-03-09 16:29:58 -0300

    projeto calculadora
    09/03

--- react/calculadora/src/components/Button.jsx
+++ react/calculadora/src/components/Button.jsx
@@ -1,6 +1,30 @@
 import React from "react";
 import './Button.css'
 
+function determinaQualBotao(props) {
+    switch (props.label) {
+        case '/':
+            return 'operador'
+        case '*':
+            return 'operador'
+        case '+':
+            return 'operador'
+        case '-':
+            return 'operador'
+        case 'AC':
+            return 'ac'
+        case '0':
+            return 'zero'
+        default:
+            return 'numero'
+    }
+}
+
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props =>
-    <button className="button">{props.label}</button>
+    <button className={`button ${determinaQualBotao(props)}`}
+        onClick={e => props.click && props.click(props.label)}
+    >{props.label}
+
+    </button>
--- react/calculadora/src/main/Calculator.jsx
+++ react/calculadora/src/main/Calculator.jsx
@@ -1,29 +1,91 @@
 import React, { Component } from "react";
 import './Calculator.css'
+import Display from "../components/Display";
 
 import Button from "../components/Button";
+import { values } from "lodash";
+
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
 
 export default class Calculator extends Component {
+    constructor(props) {
+        super(props)
+        this.clearMemory = this.clearMemory.bind(this)
+        this.setOperation = this.setOperation.bind(this)
+        this.setDigit = this.setDigit.bind(this)
+        this.state = { ...initialState }
+
+
+    }
+
+    clearMemory() {
+        this.setState({ ...initialState })
+    }
+
+    setOperation(operation) {
+        if (this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true })
+        }
+    }
+
+    setDigit(n) {
+        if (n === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+
+        if (n === '<-') {
+            let valorAtual = this.state.displayValue
+            if (valorAtual === '0') return
+            valorAtual = valorAtual.slice(0, -1)
+            if (valorAtual === '') valorAtual = '0'
+            this.setState({ displayValue: valorAtual })
+            return
+        }
+
+        const clearDisplay = this.state.displayValue === '0'
+            || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        this.setState({ displayValue, clearDisplay: false })
+
+        if (n !== '.') {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+            console.log(values)
+        }
+    }
+
     render() {
         return (
             <div className="calculator">
-                <Button label='AC' />
-                <Button label='/' />
-                <Button label='7' />
-                <Button label='8' />
-                <Button label='9' />
-                <Button label='*' />
-                <Button label='4' />
-                <Button label='5' />
-                <Button label='6' />
-                <Button label='-' />
-                <Button label='1' />
-                <Button label='2' />
-                <Button label='3' />
-                <Button label='+' />
-                <Button label='0' />
-                <Button label='.' />
-                <Button label='=' />
+                <Display value={this.state.displayValue} />
+                <Button label='AC' click={this.clearMemory} />
+                <Button label='<-' click={this.setDigit} />
+                <Button label='/' click={this.setOperation} />
+                <Button label='7' click={this.setDigit} />
+                <Button label='8' click={this.setDigit} />
+                <Button label='9' click={this.setDigit} />
+                <Button label='*' click={this.setOperation} />
+                <Button label='4' click={this.setDigit} />
+                <Button label='5' click={this.setDigit} />
+                <Button label='6' click={this.setDigit} />
+                <Button label='-' click={this.setOperation} />
+                <Button label='1' click={this.setDigit} />
+                <Button label='2' click={this.setDigit} />
+                <Button label='3' click={this.setDigit} />
+                <Button label='+' click={this.setOperation} />
+                <Button label='0' click={this.setDigit} />
+                <Button label='.' click={this.setDigit} />
+                <Button label='=' click={this.setOperation} />
             </div>
         )
     }
